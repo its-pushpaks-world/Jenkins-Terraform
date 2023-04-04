c85bd28fa5a0c0b4acd5e2ab1804f3af6b080ca7
its-pushpaks-world <115705491+its-pushpaks-world@users.noreply.github.com>
2023-04-05 00:28:44 +0530

    Update key.tf

--- key.tf
+++ key.tf
@@ -2,12 +2,12 @@ resource "tls_private_key" "key" {
   algorithm = "RSA"
 }
 resource "local_sensitive_file" "private_key" {
-  filename = "TESTING.pem"
+  filename = "TEST.pem"
   #sensitive_content = tls_private_key.key.private_key_pem
   content         = tls_private_key.key.private_key_pem
   file_permission = "0400"
 }
 resource "aws_key_pair" "key_pair" {
-  key_name   = "TESTING"
+  key_name   = "TEST"
   public_key = tls_private_key.key.public_key_openssh
 }

e29177f78d031ef17b04a0201c2810eec3f60598
its-pushpaks-world <115705491+its-pushpaks-world@users.noreply.github.com>
2023-04-06 17:45:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,8 +6,16 @@ terraform {
   }
 }
 
+provider "vault" {
+  address = "http://127.0.0.1:8200/"
+}
+
+data "vault_generic_secret" "demo" {
+  path = "secret/pushpak-aws"
+}
+
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAWI764HXNGGEPQXGE"
-  secret_key = "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"
+  access_key = data.vault_generic_secret.demo.data["access_key"]
+  secret_key = data.vault_generic_secret.demo.data["secret_key"]
 }
